baaebcfed0004fc6aabd30effaef43d0c61765be
JuanJoseTJ29 <juan.tirado@unmsm.edu.pe>
2022-09-09 04:21:31 -0500

    edit y deleted tareas v1.0

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('',views.ingresar,name='ingresar'),
     path('dashboard',views.dashboard,name='dashboard'),
     path('nuevaTarea',views.nuevaTarea,name='nuevaTarea'),
+    path('editarTarea/<str:ind>',views.editarTarea,name='editarTarea'),
+    path('eliminarTarea/<str:ind>',views.eliminarTarea,name='eliminarTarea'),
    # path('hola',views.hola,name='hola'),
    # path('hastaluego',views.hastaluego,name='hastaluego'),
 
--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -43,7 +43,7 @@ def dashboard(request):
 
      #Filtrar finalizado
     return render(request, 'gestion_tareas/dashboard.html', {
-        'objTarea':tareas_totales,
+        'objTarea':tareas_propias,
     })
 
 def nuevaTarea(request):
@@ -59,5 +59,41 @@ def nuevaTarea(request):
         tarea(nombre_tarea=nombreTarea,descripcion=descripcionTarea,fecha_creacion=fcTarea, fecha_entrega=feTarea , usuario_responsable=usuarioTarea, estadoTarea=estadosTareas).save()
         return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
     return render(request,'gestion_tareas/nuevaTarea.html',{
+        'tareas_registradas':tarea.objects.all()
+    })
+
+
+def editarTarea(request,ind):
+    tarea_editar = tarea.objects.get(id=ind)
+    if request.method == 'POST':
+        nombreTarea=request.POST.get('nombreTarea')
+        descripcionTarea=request.POST.get('descripcionTarea')
+        fcTarea=request.POST.get('fcTarea')
+        fcTarea=parse(fcTarea)
+        feTarea=request.POST.get('feTarea')
+        feTarea=parse(feTarea)
+        usuarioTarea=request.POST.get('usuarioTarea')
+        estadosTareas=request.POST.get('estadosTareas')    
+
+        tarea_editar.nombre_tarea = nombreTarea
+        tarea_editar.descripcion = descripcionTarea
+        tarea_editar.fecha_creacion = fcTarea
+        tarea_editar.fecha_entrega = feTarea
+        tarea_editar.usuario_responsable= usuarioTarea
+        tarea_editar.estadoTarea= estadosTareas
+        tarea_editar.save()
+        return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
+    return render(request,'gestion_tareas/editarTarea.html',{
+        'tarea_info' : tarea_editar,
+        'tareas_registradas':tarea.objects.all()
+    })
+
+def eliminarTarea(request,ind):
+    tarea_eliminar = tarea.objects.get(id=ind)
+    if request.method == 'POST':
+        tarea_eliminar.delete()
+        return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
+    return render(request,'gestion_tareas/eliminarTarea.html',{
+        'tarea_info' : tarea_eliminar,
         'tareas_registradas':tarea.objects.all()
     })
